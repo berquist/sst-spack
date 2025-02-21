23789a754b4c0793687b687c280a0576ed531c62
Eric Berquist <ejberqu@sandia.gov>
2025-02-21 08:57:59 -0700

    verbose run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# shellcheck disable=SC2086
+# https://web.archive.org/web/20230401201759/https://wiki.bash-hackers.org/scripting/debuggingtips#making_xtrace_more_useful
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+set -x
+
 set -euo pipefail
 
 eval "$(spack env activate --sh -d .)"
