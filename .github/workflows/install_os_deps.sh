061052c36d7c0ff7b65ef988a6cc7b766a6b9dcf
Eric Berquist <ejberqu@sandia.gov>
2025-03-07 10:07:21 -0700

    better handling of OS-level dependencies

--- .github/workflows/install_os_deps.sh
+++ .github/workflows/install_os_deps.sh
@@ -5,11 +5,18 @@
 
 set -euo pipefail
 
+export DEBIAN_FRONTEND=noninteractive
 if command -v brew; then
     brew list
     # Problems with Python overwriting files in /usr/local/bin
     # brew update
     # brew upgrade
+elif command -v dnf >/dev/null 2>&1; then
+    dnf install -y python3.12
 elif command -v apt-get; then
-    sudo apt-get -y --no-install-recommends update
+    apt-get update -y --no-install-recommends
+    apt-get install -y --no-install-recommends python3
+elif command -v pacman >/dev/null 2>&1; then
+    pacman -Syu
+    pacman -S --noconfirm python
 fi
