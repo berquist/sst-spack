f7534ae072613b53b1b553704ffeb62daa99f755
Eric Berquist <ejberqu@sandia.gov>
2025-03-11 09:47:01 -0600

    add Rocky 8 and 9

--- .github/workflows/install_os_deps.sh
+++ .github/workflows/install_os_deps.sh
@@ -12,7 +12,11 @@ if command -v brew; then
     # brew update
     # brew upgrade
 elif command -v dnf >/dev/null 2>&1; then
-    dnf install -y python3.12
+    dnf upgrade -y
+    dnf install -y \
+        python3.12 \
+        python3.12-pip \
+        python3.12-pyyaml
 elif command -v apt-get; then
     apt-get update -y --no-install-recommends
     apt-get install -y --no-install-recommends python3
