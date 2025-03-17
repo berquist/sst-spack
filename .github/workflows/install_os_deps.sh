926ccb4a9772a154ced80628171b62caad2120a6
Eric Berquist <ejberqu@sandia.gov>
2025-03-17 10:31:19 -0600

    install compilers on Rocky

--- .github/workflows/install_os_deps.sh
+++ .github/workflows/install_os_deps.sh
@@ -14,6 +14,8 @@ if command -v brew; then
 elif command -v dnf >/dev/null 2>&1; then
     dnf upgrade -y
     dnf install -y \
+        g++ \
+        gfortran \
         git \
         python3.12 \
         python3.12-pip \
