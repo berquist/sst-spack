b100f01aac8008140dc2ac6088ef8d10ea31001f
Eric Berquist <ejberqu@sandia.gov>
2025-03-21 09:01:08 -0600

    install more OS-level dependencies

--- .github/workflows/install_os_deps.sh
+++ .github/workflows/install_os_deps.sh
@@ -15,18 +15,25 @@ if command -v brew; then
 elif command -v dnf >/dev/null 2>&1; then
     dnf upgrade -y
     dnf install -y \
+        bzip2 \
         gcc-c++ \
         gcc-gfortran \
         git \
         patch \
         python3.12 \
         python3.12-pip \
-        python3.12-pyyaml
+        python3.12-pyyaml \
+        unzip \
+        xz
 elif command -v apt-get; then
     apt-get update -y --no-install-recommends
     apt-get install -y --no-install-recommends \
+            bzip2 \
+            git \
             python3 \
-            python3-yaml
+            python3-yaml \
+            unzip \
+            xz-utils
 elif command -v pacman >/dev/null 2>&1; then
     pacman -Syu --noconfirm
     pacman -S --noconfirm \
